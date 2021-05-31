7cd7d8aa4535b0752035ab6e1e494fa13780fb88
Tyler Freedman <mail@tylerfreedman.com>
2021-05-31 08:58:02 -0400

    First build

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  def home
+    if params[:year].nil?
+      date = (Date.today - 1.year)
+    else
+      date = Date.parse("#{params[:year]}-#{params[:month]}-#{params[:day]}")
+    end
+
+    if params[:subreddit]
+      subreddit = "&subreddit=#{params[:subreddit]}"
+    else
+      subreddit = ""
+    end
+
+    @url = "https://api.pushshift.io/reddit/submission/search?after=#{date.strftime}&before=#{(date+1.day).strftime}&sort=desc&sort_type=score#{subreddit}"
+    begin
+      @output = JSON.parse(HTTParty.get(@url).body)
+    rescue
+      @output = nil
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/:year-:month-:day' => 'application#home'
+  get '/:year-:month-:day/r/:subreddit' => 'application#home'
+  root 'application#home'
 end
